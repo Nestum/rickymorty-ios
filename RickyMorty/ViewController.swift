9670ae49a5fe83e2971c082a7e0282e4c7f87149
Nestum <marco.castanheira@gmail.com>
2022-01-29 18:34:08 +0000

    Inicial commit

---
+++ RickyMorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickyMorty
+//
+//  Created by Marco Castanheira on 29/01/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
